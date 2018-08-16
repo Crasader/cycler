00ff1b6a6abcc1874495bcc6aafa17c9df83a48e
Магомедали <web-ali@yandex.ru>
2018-08-16 11:32:38 +0300

    1.0.0

--- app/Http/Controllers/Api/ApiSelfController.php
+++ app/Http/Controllers/Api/ApiSelfController.php
@@ -32,7 +32,9 @@ class ApiSelfController extends Controller
 
 
         $answer = array();
+        
         $roles = Role::all();
+        
         foreach ($roles as $role) {
             $answer['roles'][$role->name]['title'] = $role->display_name;
             $answer['roles'][$role->name]['permissions'] = $role->perms()->get(['name','display_name'])->toArray();
@@ -48,11 +50,7 @@ class ApiSelfController extends Controller
         //Роли пользователя
         $answer['current_user']['roles'] = $user->roles()->get(['name','display_name'])->toArray();
 
-        //Справочники
-        $answer['app_settings'] = array();
-
-        //Справочники
-        $answer['dictionaries'] = array();
+        
 
         //схема таблиц
         $deals = new Deals();
