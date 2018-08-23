e10a960982854ccfffe7adaa6f604a5666ddc649
Магомедали <web-ali@yandex.ru>
2018-08-23 13:24:33 +0300

    add cors headers

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -14,13 +14,52 @@ class Cors
      * @return mixed
      */
     public function handle($request, Closure $next)
+    {    
+        
+        $resp =  1 ? $next($request) : $this->corsHandle($request,$next);
+
+        return $resp;
+    }
+    
+
+
+
+
+
+    
+    protected function corsHandle($request, Closure $next){
+
+        if (! $this->isCorsRequest($request)) {
+            return $next($request);
+        }
+        
+        if($request->getMethod() === 'OPTIONS'){
+            $response = response('Preflight OK', 200);
+            
+            $response->headers->set('Access-Control-Allow-Methods',  'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+            $response->headers->set('Access-Control-Allow-Headers', "Origin, Authorization, Content-Type, X-Auth-Token");
+            $response->headers->set('Access-Control-Allow-Origin', "*");
+            $response->headers->set('Access-Control-Max-Age', 60 * 60 * 24);
+            
+            return $response;
+        }
+        
+        
+        $response = $next($request);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Expose-Headers', 'Cache-Control, Content-Language, Content-Type, Expires, Last-Modified, Pragma');
+        
+        
+        return $response;
+
+    }
+    
+    protected function isCorsRequest($request): bool
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Request-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
-             
-            //->header('Access-Control-Allow-Credentials', true);
+        if (! $request->headers->has('Origin')) {
+            return false;
+        }
+
+        return $request->headers->get('Origin') !== $request->getSchemeAndHttpHost();
     }
 }
